3b858007b8341a0bcc68b57568b73a90542b0a82
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-02-28 00:47:03 +0900

    20190304-20190310

--- script.sh
+++ script.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-php index.php 120 &&
-mv result.txt session_id_20190304.txt &&
-
-php index.php 160 &&
-mv result.txt session_id_20190305.txt &&
-
-php index.php 160 &&
-mv result.txt session_id_20190306.txt &&
-
-php index.php 160 &&
-mv result.txt session_id_20190307.txt &&
-
-php index.php 160 &&
-mv result.txt session_id_20190308.txt &&
-
-php index.php 280 &&
-mv result.txt session_id_20190310.txt &&
-
 php index.php 160 &&
 mv result.txt session_id_20190311.txt &&
 
