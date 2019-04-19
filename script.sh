898984d75e50d96de6f33d26ffcf476ea80376cf
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-04-20 02:00:28 +0900

    fix shell

--- script.sh
+++ script.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_ara.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_ara.txt &&
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_ishikawa.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_ishikawa.txt &&
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_ishiwata.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_ishiwata.txt &&
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_kawamura.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_kawamura.txt &&
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_hamakawa.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_hamakawa.txt &&
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_yamamoto.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_yamamoto.txt &&
 
-php index.php 80 &&
-mv result.txt session_id_201904_3_takahashi.txt &&
+php index.php 20 &&
+mv result.txt session_id_201904_4_takahashi.txt &&
 
 
 exit 0
