702e933b3f6fad37032c4b3a0c8a86b504d2a874
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-06-21 10:17:19 +0900

    add session_id break_through

--- script.sh
+++ script.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
 php index.php 60 &&
-mv result.txt session_id_201906_1_ara.txt &&
+mv result.txt session_id_breakt_through_yoshida.txt &&
 
 php index.php 60 &&
-mv result.txt session_id_201906_1_ishikawa.txt &&
+mv result.txt session_id_breakt_through_niizawa.txt &&
 
 php index.php 60 &&
-mv result.txt session_id_201906_1_ishiwata.txt &&
-
-php index.php 30 &&
-mv result.txt session_id_201906_1_kawamura.txt &&
+mv result.txt session_id_breakt_through_ito.txt &&
 
 php index.php 60 &&
-mv result.txt session_id_201906_1_hamakawa.txt &&
+mv result.txt session_id_breakt_through_kujirai.txt &&
 
-php index.php 30 &&
-mv result.txt session_id_201906_1_yamamoto.txt &&
-
-php index.php 30 &&
-mv result.txt session_id_201906_1_takahashi.txt &&
 
 
 exit 0
