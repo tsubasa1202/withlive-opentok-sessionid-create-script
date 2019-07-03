597278e6cb3a063a2d0325e159e64a6b92577f7a
Tsubasa Oshima <w.tsubasa.physics@gmail.com>
2019-07-04 02:17:46 +0900

    スクガムCDキャンペーン

--- script.sh
+++ script.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-php index.php 60 &&
-mv result.txt session_id_breakt_through_yoshida.txt &&
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_ishikawa.txt &&
 
-php index.php 60 &&
-mv result.txt session_id_breakt_through_niizawa.txt &&
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_ara.txt &&
 
-php index.php 60 &&
-mv result.txt session_id_breakt_through_ito.txt &&
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_hamakawa.txt &&
 
-php index.php 60 &&
-mv result.txt session_id_breakt_through_kujirai.txt &&
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_ishiwata.txt &&
+
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_yamamoto.txt &&
+
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_kawamura.txt &&
+
+php index.php 300 &&
+mv result.txt session_id_cd_campaign_takatashi.txt &&
 
 
 
